f73c3b4573800db4abd5e74b4c4f3b5f0aa86807
sanjith <vgsanjith166@gmail.com>
2024-04-08 23:41:30 +0530

    Lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import CartPage from './components/CartPage';
+// import CartPage from './components/CartPage';
 import ErrorPage from './components/ErrorPage';
 import ProductListPage from './components/ProductListPage';
 import Header from './components/Header';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { Suspense, lazy } from 'react';
 
-function App() {
+
+
+
+const App = ()=>{  
+
+  const cartLoading = lazy(() => import('./components/CartPage'));
   return (
 
     <Provider store={store}>
@@ -17,7 +23,11 @@ function App() {
     <Header />
     <Routes>
       <Route path="/" element={<ProductListPage />}></Route>
-      <Route path="/cart" element={<CartPage />}></Route>
+      <Route path="/cart" element={
+        <Suspense fallback={"Loading ..."}>
+            <cartLoading />
+        </Suspense>
+      }></Route>
       <Route path='*' element={<ErrorPage/>}></Route>
     </Routes>
     </BrowserRouter>
